9cbfe7a7f3cc626832b910150ffdb28ff69a2b97
Sam King <mail@samking.co>
2021-10-08 22:46:22 +0100

    Update get single token endpoint

--- pages/api/flowers/[tokenId].ts
+++ pages/api/flowers/[tokenId].ts
@@ -1,34 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import flowersData from "../../../data/ranked-flower-images.json";
-import { Flower, ResponseError } from "../../../utils";
+import { getFlower } from "occ-flowers-sdk";
+import { Flower } from "occ-flowers-sdk/dist/types";
+import { ResponseError } from "../../../utils";
 
 export interface FlowerResponse extends Flower {}
-
-const flowers = flowersData as Flower[];
 
 export default async (
   req: NextApiRequest,
   res: NextApiResponse<FlowerResponse | ResponseError>
 ) => {
   const tokenId = req.query.tokenId as string;
-  const flower = flowers.find((i) => i.tokenId.toString() === tokenId);
+  let flower;
 
-  if (!flower) {
+  try {
+    flower = await getFlower(parseInt(tokenId));
+  } catch (e) {
     res.status(404).json({
       message: `No flower found with 'tokenId' ${tokenId}`,
       status: 404,
     });
     return;
   }
-
-  const { base64 } = flower.image;
-  const html = Buffer.from(base64.substring(26), "base64").toString();
-
-  res.status(200).json({
-    ...flower,
-    image: {
-      html,
-      base64,
-    },
-  });
+  
+  res.status(200).json(flower);
 };
